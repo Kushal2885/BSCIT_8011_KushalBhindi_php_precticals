aa869d5c868966d88f77c3e6f9fe79c674f7f5cb
Kushal Bhindi <kushalsoni2928@gmail.com>
2026-06-09 11:16:19 +0530

    Update p8.php

--- Unit-1/p8.php
+++ Unit-1/p8.php
@@ -3,9 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Program 8 | 04/06/2026</title>
+    <title>Program 8 | 09/06/2026</title>
 </head>
 <body>
-    
+    <?php
+        error_reporting(0);
+        include 'header.php';
+    ?>
+
+    <form method="get">
+    <b>Enter array values (comma-separated):</b><br>
+    <input type="text" name="numbers" required>
+    <input type="submit" value="Submit">
+</form>
+
+    <?php
+        $nm = $_GET['numbers'];
+        echo "The Array is: " . $nm;
+        require 'footer.php';
+    ?>
 </body>
 </html>
